e71fa9d67532057e829416fd9ce7aa59b8646e2c
matheusdemo <114447940+matheusdemo@users.noreply.github.com>
2022-10-19 19:11:46 -0300

    comando de repetição

---
+++ while em c.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int contador=0;//declaraçao de variável
+    while(contadoe<=100)//estrutura de repetição utilizando whilw
+    {
+    printf("%d", contador);//Exibe Exibe a contagem na tela
+    contador--; //incremento do contador
+    }
+    return 0;
+}
